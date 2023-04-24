6e1853194dee90da569e52f933612068ed7b486c
trevorfitz0 <trevorfitz0@gmail.com>
2023-04-24 09:30:54 -0600

    User can now enter a name and select an order, these values are sotred in states

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -22,7 +22,16 @@ class OrderForm extends Component {
     this.setState({name: '', ingredients: []});
   }
 
+  handleIngredientChange = (e) => {
+    e.preventDefault()
+    const newArr = this.state.ingredients
+    newArr.push(e.target.name)
+    this.setState({ ingredients: newArr })
+  }
 
+  handleNameChange = (e) => {
+    this.setState({ name: e.target.value})
+  }
 
   render() {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
