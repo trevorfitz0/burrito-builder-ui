abda43d74c559b40f50988f0f7a0650becc65d2a
trevorfitz0 <trevorfitz0@gmail.com>
2023-04-25 10:00:56 -0600

    Key Fix

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -4,11 +4,11 @@ import './Orders.css';
 const Orders = props => {
   const orderEls = props.orders.map(order => {
     return (
-      <div key={order.id} className="order">
+      <div key={order.id + Math.random(Math.floor() * 1000)} className="order">
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li key={ingredient}>{ingredient}</li>
+            return <li key={ingredient + order.id}>{ingredient}</li>
           })}
         </ul>
         <button onClick={() => props.remove(order.id) }>Complete!</button>
